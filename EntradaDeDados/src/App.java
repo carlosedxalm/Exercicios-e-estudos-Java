f2b139adad97c606a9dea0b59f51a07e542c0c36
carlosedxalm <carlosedx.alm@gmail.com>
2026-02-06 11:30:14 -0300

    esses são meus primeiros exercicios de fixição em Java, entrada e saida de dados, estrutura sequencial

---
+++ EntradaDeDados/src/App.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int x;
+
+        x = sc.nextInt();
+
+        System.out.println("Voce digitou: " + x);
+
+        sc.close();
+
+
+    }
+}
