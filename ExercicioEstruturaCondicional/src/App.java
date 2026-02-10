e8c5a11d331b22649aa60b6962e6ca1a2b7dbe1d
carlosedxalm <carlosedx.alm@gmail.com>
2026-02-10 09:42:41 -0300

    Exercicio simples, focado em um if-else básico de uma estrutura condicional.

--- ExercicioEstruturaCondicional/src/App.java
+++ ExercicioEstruturaCondicional/src/App.java
@@ -2,11 +2,15 @@ import java.util.Locale;
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
+        // Configura o locale para garantir que o ponto seja usado como separador decimal
         Locale.setDefault(new Locale("en", "US"));
+        // Cria um Scanner para ler a entrada do usuário
         Scanner sc = new Scanner(System.in);
 
+        // Declaração das variáveis para armazenar as notas do aluno
         double nota1, nota2;
 
+        // Solicita ao usuário que digite as duas notas do aluno
         System.err.println("Digite as duas notas do aluno: ");
         System.err.println("Nota 1: ");
         nota1 = sc.nextDouble();
@@ -15,10 +19,12 @@ public class App {
         double notaFunal = nota1 + nota2;
         System.err.println("Nota final: " + notaFunal);
 
+        // Verifica se a nota final é menor que 60.0 para determinar se o aluno está aprovado ou reprovado
         if (notaFunal < 60.0) {
             System.err.println("Reprovado");
         } else {
             System.err.println("Aprovado");
+            
         }
         sc.close();
     }
