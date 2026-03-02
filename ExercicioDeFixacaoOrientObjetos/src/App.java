d5dd2282118c5ecd6c6c1046e922ea525eba0c5c
carlosedxalm <carlosedx.alm@gmail.com>
2026-03-02 10:12:37 -0300

    Implementação da logica do programa base, e requisições da classe, além de remoção de declarações de impoort e package desnecessarias, finalização do programa.

--- ExercicioDeFixacaoOrientObjetos/src/App.java
+++ ExercicioDeFixacaoOrientObjetos/src/App.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Employee emp = new Employee();
+
+        System.out.print("Name: ");
+        emp.name = sc.nextLine();
+        System.out.print("Gross Salary: ");
+        emp.grossSalary = sc.nextDouble();
+        System.out.print("Tax: ");
+        emp.tax = sc.nextDouble();
+
+        System.out.println();
+
+        System.out.println("Employee: " + emp);
+        System.out.println();
+        System.out.print("Which percentage to increase salary? ");
+        double percentage = sc.nextDouble();
+        emp.increaseSalary(percentage);
+
+        System.out.println();
+
+        System.out.println("Updated data: " + emp);
+        sc.close();
     }
 }
